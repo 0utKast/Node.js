6f11b26da842e6f6b6149336b3b47fa18a51cb5b
0utKast <educaleft@gmail.com>
2012-11-01 22:14:30 +0000

    videotutorial19
    Prácticas incluidas del Videotutorial Nº 19 del Curso de Node.js

---
+++ VT19_conectar_a_mongo/app.js
@@ -0,0 +1,47 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , mongoose = require('mongoose');
+ 
+ mongoose.connect('mongodb://localhost/desarrollo_tareas')
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Tarea = new Schema({
+  tarea: String
+});
+
+var Tarea = mongoose.model('Tarea', Tarea);
+
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
