2ef26266d526a1a3b2b2e5b25b9c623b5eae670e
0utKast <educaleft@gmail.com>
2012-11-03 13:11:06 +0000

    vt20node
    Envío material prácticas videotutorial nº 20 Curso de GitHub

---
+++ VT20_conectar_a_mongo/app.js
@@ -0,0 +1,74 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , mongoose = require('mongoose');
+ 
+ mongoose.connect('mongodb://localhost/desarrollo_tareas')
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Tarea = new Schema({
+  tarea: String
+});
+
+var Tarea = mongoose.model('Tarea', Tarea);
+
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/tareas', function(req, res){
+  Tarea.find({}, function (err, docs) {
+    res.render('tareas/index', {
+      title: 'Vista index lista de tareas',
+      docs: docs
+    });
+  });
+});
+app.get('/tareas/nueva', function(req, res){
+  res.render('tareas/nueva.jade', {
+    title: 'Nueva Tarea'
+  });
+});
+
+app.post('/tareas', function(req, res){
+  var tarea = new Tarea(req.body.tarea);
+  tarea.save(function (err) {
+    if (!err) {
+      res.redirect('/tareas');
+    }
+    else {
+      res.redirect('/tareas/nueva');
+    }
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
+
